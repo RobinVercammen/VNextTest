bc9bfb87ef86f2e5fc0159ad9261253602d2e39c
Bart <bart.corremans@gmail.com>
2015-09-07 12:38:05 +0200

    Change bower location, add app.js

--- WebApp/Scripts/app.js
+++ WebApp/Scripts/app.js
@@ -1,1 +1,27 @@
-﻿
+﻿var vNextApp = angular.module('vNextApp', [
+    'ngRoute',
+
+    'vNextApp.services',
+    'vNextApp.controllers'
+]);
+
+angular.module('vNextApp.services', []);
+angular.module('vNextApp.controllers', []);
+
+vNextApp.config(['$logProvider', '$routeProvider',
+    function ($logProvider, $routeProvider) {
+        // Logging
+        $logProvider.debugEnabled(true);
+
+        // Route config
+        $routeProvider.
+            otherwise({
+                redirectTo: '/'
+            });
+    }
+]);
+
+vNextApp.run(['$rootScope',
+    function ($rootScope) {
+        console.log('Hi mum');
+    }]);

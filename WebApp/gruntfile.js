bfd622e82cfaecd58d49a47d8ed944c5a5fdf31b
Bart <bart.corremans@gmail.com>
2015-09-07 14:01:26 +0200

    Set up grunt

--- WebApp/gruntfile.js
+++ WebApp/gruntfile.js
@@ -4,5 +4,44 @@ Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x
 */
 module.exports = function (grunt) {
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            js: {
+                options: {
+                    separator: ';'
+                },
+                src: [
+                    'Scripts/app.js',
+                    'Scripts/services/**/*.js',
+                    'Scripts/controllers/**/*.js',
+                    'Scripts/components/*/*.js',
+                    'Scripts/components/**/*.js',
+                ],
+                dest: 'dist/<%= pkg.name %>.js'
+            },
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.js.dest %>'] // take the output of the concat task and minify it
+                }
+            }
+        },
+        watch: {
+            dev: {
+                files: ['gruntfile.js', 'Scripts/**/*.js'],
+                tasks: ['concat:js', 'uglify'],
+                options: {
+                    atBegin: true
+                }
+            }
+        }
     });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('dev', ['watch:dev']);
 };
